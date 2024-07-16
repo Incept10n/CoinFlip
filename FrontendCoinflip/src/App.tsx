1180adde64f760a5125789c69b1a84997dd8e325
Incept1on <a.d.mihailik@yandex.ru>
2024-07-16 10:49:09 +0300

    Initial commit

---
+++ FrontendCoinflip/src/App.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { TonConnectButton } from '@tonconnect/ui-react'
+// import { useMainContract } from './hooks/useMainContract';
+import { useTonConnect } from './hooks/useTonconnect';
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
